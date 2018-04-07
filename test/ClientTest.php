05295f7d3b6454169083e985e1314c282dde96ce
Simon Verraest <simon@appfleet.uk>
2018-04-08 00:10:30 +0100

    Added more test coverage

--- test/ClientTest.php
+++ test/ClientTest.php
@@ -36,10 +36,65 @@ class ClientTest extends PHPUnit_Framework_TestCase
         $http_client = new Client(['handler' => $stack]);
         $client = new RevolutPHP\Client('foo' , 'production');
         $client->setClient($http_client);
+
         $client->accounts->all();
+
         foreach ($container as $transaction) {
            $this->assertEquals('GET', $transaction['request']->getMethod());
-           $this->assertEquals('b2b.revolut.com', $transaction['request']->getHeaders()['Host'][0]);
+        }
+    }
+
+    public function testGet()
+    {
+        $mock = new MockHandler([new Response(200, ['X-Foo' => 'Bar'], "{\"foo\":\"bar\"}")]);
+        $container = [];
+        $history = Middleware::history($container);
+        $stack = HandlerStack::create($mock);
+        $stack->push($history);
+        $http_client = new Client(['handler' => $stack]);
+        $client = new RevolutPHP\Client('foo' , 'production');
+        $client->setClient($http_client);
+
+        $client->accounts->get('foo');
+
+        foreach ($container as $transaction) {
+            $this->assertEquals('GET', $transaction['request']->getMethod());
+        }
+    }
+
+    public function testPost()
+    {
+        $mock = new MockHandler([new Response(200, ['X-Foo' => 'Bar'], "{\"foo\":\"bar\"}")]);
+        $container = [];
+        $history = Middleware::history($container);
+        $stack = HandlerStack::create($mock);
+        $stack->push($history);
+        $http_client = new Client(['handler' => $stack]);
+        $client = new RevolutPHP\Client('foo' , 'production');
+        $client->setClient($http_client);
+
+        $client->counterparties->create(['foo' => 'bar']);
+
+        foreach ($container as $transaction) {
+            $this->assertEquals('POST', $transaction['request']->getMethod());
+        }
+    }
+
+    public function testDelete()
+    {
+        $mock = new MockHandler([new Response(200, ['X-Foo' => 'Bar'], "{\"foo\":\"bar\"}")]);
+        $container = [];
+        $history = Middleware::history($container);
+        $stack = HandlerStack::create($mock);
+        $stack->push($history);
+        $http_client = new Client(['handler' => $stack]);
+        $client = new RevolutPHP\Client('foo' , 'production');
+        $client->setClient($http_client);
+
+        $client->counterparties->delete('foo');
+
+        foreach ($container as $transaction) {
+            $this->assertEquals('DELETE', $transaction['request']->getMethod());
         }
     }
 }
