ea8c2b00709ac39db251730fd22c2a931ff9ced0
Bart <bartosz@cloudtrainer.co.uk>
2019-05-24 22:39:48 +0100

    add exchange and rate api

---
+++ src/Exchanges.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace RevolutPHP;
+
+class Exchanges
+{
+    const ENDPOINT = 'exchange';
+
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * Exchanges constructor.
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * @see https://revolutdev.github.io/business-api/#exchange-currency
+     *
+     * @param array $json
+     * @return mixed
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     */
+    public function exchange(array $json)
+    {
+        return $this->client->post(self::ENDPOINT, $json);
+    }
+}
